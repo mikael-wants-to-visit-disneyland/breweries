b6efe533cfcc6effd2dd3ef316dbc2f0766205e3
Mikael <mikael@Mikaels-MBP.lan>
2022-10-01 10:36:18 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const PAGE_SIZE = 8;
+const API_URL = `https://api.openbrewerydb.org/breweries?per_page=${PAGE_SIZE}`;
 
 function App() {
+  const [breweries, setBreweries] = React.useState<[]>([]);
+  React.useEffect(() => {
+    getAndSetBreweries();
+  }, []);
+  const getAndSetBreweries = async () =>
+    fetch(API_URL)
+      .then((result) => result.json())
+      .then((bs) => setBreweries(bs))
+      .catch((err) => console.error(err));
+  console.log(breweries);
   return (
     <div className="App">
       <header className="App-header">
